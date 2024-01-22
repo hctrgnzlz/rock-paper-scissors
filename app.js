fc8e4cc3f1b8d61788a2a4d298819ae98db81305
hctrgnzlz <hectorgzz89@gmail.com>
2024-01-22 13:55:41 -0500

    rest parameters

--- app.js
+++ app.js
@@ -89,3 +89,32 @@ startGameBtn.addEventListener('click', () => {
   alert(message);
   gameIsRunning = false;
 });
+
+
+//not related to the game
+
+const sumUp = (...numbers) => {
+  const validateNumber = (number) => {
+    return isNaN(number) ? 0 : number;
+  };
+
+
+  let sum = 0;
+  for (const num of numbers) {
+    sum += validateNumber(num);
+  }
+  return sum;
+};
+
+const subtractUp = function() {
+  let sum = 0;
+  for (const num of arguments) { //don't use arguments operator
+    sum -= num;
+  }
+  return sum;
+}
+
+
+console.log(sumUp(1, 5, 10, -3, 6, 10));
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88));
+console.log(subtractUp(1, 10, 15, 20));
